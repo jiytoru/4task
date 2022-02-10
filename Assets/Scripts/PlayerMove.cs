3c8a64153edb5482d41425322266f99b7300760c
jiytoru <semblavr@gmail.com>
2022-02-10 17:01:01 +0600

    дз по 4 уроку
    я добавила бомбу и турель из предыдущего дз

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speedP;
+    [SerializeField]
+    public float speedR;
+    [SerializeField] private Vector3 direction;
+    private Rigidbody rb;
+    private CapsuleCollider _collider;
+    Quaternion p_Rot;
+    [SerializeField] private float thrust;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _collider = GetComponent<CapsuleCollider>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        dir();
+        Movement();
+        Jump();
+    }
+
+    void dir()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+        direction.Set(direction.x, 0f, direction.z);
+        var _speed = direction * speedP * Time.fixedDeltaTime;
+        transform.Translate(_speed);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, direction, speedR * Time.deltaTime, 0f);
+        p_Rot = Quaternion.LookRotation(desiredForward);
+
+    }
+
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + direction * Time.deltaTime);
+        rb.MoveRotation(p_Rot);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * thrust,ForceMode.VelocityChange);
+        }
+    }
+
+
+
+}
